9336e2fe7cfa2adf52c63b39eb9bc9499e730ab6
Mathieu Raux <mathieu_raux@hotmail.fr>
2019-02-13 12:29:09 +0100

    Amelioration pour samba

--- script/viking_function.sh
+++ script/viking_function.sh
@@ -14,61 +14,67 @@ function initmenu() {
 }
 
 function samba_installation() {
-	yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	echo -e "###########################################################\n
-############### Installation de Samba 4 AD ################\n
-###########################################################\n"
-	yum install attr bind-utils docbook-style-xsl gcc gdb krb5-workstation \
-	       libsemanage-python libxslt perl perl-ExtUtils-MakeMaker \
-	       perl-Parse-Yapp perl-Test-Base pkgconfig policycoreutils-python \
-	       python2-crypto gnutls-devel libattr-devel keyutils-libs-devel \
-	       libacl-devel libaio-devel libblkid-devel libxml2-devel openldap-devel \
-	       pam-devel popt-devel python-devel readline-devel zlib-devel systemd-devel \
-	       lmdb-devel jansson-devel gpgme-devel pygpgme libarchive-devel
-	if [ ! -d /tmp/samba ]; then
-		mkdir /tmp/samba
+	if [ ! -e /usr/local/samba/sbin/samba ]; then
+		yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+		echo -e "###########################################################\n
+	############### Installation de Samba 4 AD ################\n
+	###########################################################\n"
+		yum install attr bind-utils docbook-style-xsl gcc gdb krb5-workstation \
+		       libsemanage-python libxslt perl perl-ExtUtils-MakeMaker \
+		       perl-Parse-Yapp perl-Test-Base pkgconfig policycoreutils-python \
+		       python2-crypto gnutls-devel libattr-devel keyutils-libs-devel \
+		       libacl-devel libaio-devel libblkid-devel libxml2-devel openldap-devel \
+		       pam-devel popt-devel python-devel readline-devel zlib-devel systemd-devel \
+		       lmdb-devel jansson-devel gpgme-devel pygpgme libarchive-devel winbind
+		if [ ! -d /tmp/samba ]; then
+			mkdir /tmp/samba
+		else
+			rm -rf /tmp/samba
+			mkdir /tmp/samba
+		fi
+		cd /tmp/samba
+		wget https://download.samba.org/pub/samba/stable/samba-4.9.4.tar.gz
+		tar xvf samba-4.9.4.tar.gz
+		cd samba-4.9.4
+		./configure
+		if [ $? = 0 ]; then
+			make
+			make install
+		else
+			echo "ERREUR : La configuration de samba a échoué"
+		fi
+		if [ -d /usr/local/samba/bin ]; then
+			echo "Réglage du pare-feu"
+			firewall-cmd --permanent --add-port=53/udp
+			firewall-cmd --permanent --add-port=53/tcp
+			firewall-cmd 
+			firewall-cmd --reload
+
+			echo "Création du démon"
+			systemctl mask smbd nmbd winbind
+			systemctl disable smbd nmbd winbind
+
+			echo '[Unit]
+			Description=Samba Active Directory Domain Controller
+			After=network.target remote-fs.target nss-lookup.target
+
+			[Service]
+			Type=forking
+			ExecStart=/usr/local/samba/sbin/samba -D
+			PIDFile=/usr/local/samba/var/run/samba.pid
+			ExecReload=/bin/kill -HUP $MAINPID
+
+			[Install]
+			WantedBy=multi-user.target' > /etc/systemd/system/samba-ad-dc.service
+
+			systemctl enable samba-ad-dc
+
+			whiptail --title "Installation de Samba" --msgbox "L'installation de Samba est terminé" 10 60
+			samba_configuration
+		fi
 	else
-		rm -rf /tmp/samba
-		mkdir /tmp/samba
-	fi
-	cd /tmp/samba
-	wget https://download.samba.org/pub/samba/stable/samba-4.9.4.tar.gz
-	tar xvf samba-4.9.4.tar.gz
-	cd samba-4.9.4
-	./configure
-	if [ $? = 0 ]; then
-		make
-		make install
-	else
-		echo "ERREUR : La configuration de samba a échoué"
-	fi
-	if [ -d /usr/local/samba/bin ]; then
-		echo "Réglage du pare-feu"
-		firewall-cmd --permanent --add-port=53/udp
-		firewall-cmd --permanent --add-port=53/tcp
-		firewall-cmd 
-		firewall-cmd --reload
-
-		echo "Création du démon"
-		systemctl mask smbd nmbd winbind
-		systemctl disable smbd nmbd winbind
-
-		echo '[Unit]
-		Description=Samba Active Directory Domain Controller
-		After=network.target remote-fs.target nss-lookup.target
-
-		[Service]
-		Type=forking
-		ExecStart=/usr/local/samba/sbin/samba -D
-		PIDFile=/usr/local/samba/var/run/samba.pid
-		ExecReload=/bin/kill -HUP $MAINPID
-
-		[Install]
-		WantedBy=multi-user.target' > /etc/systemd/system/samba-ad-dc.service
-
-		systemctl enable samba-ad-dc
-
-		whiptail --title "Installation de Samba" --msgbox "L'installation de Samba est terminé" 10 60
+		whiptail --title "Installation de Samba" --msgbox "Samba est déjà installé sur ce serveur" 10 60
+		initmenu
 	fi
 }
 
@@ -92,4 +98,7 @@ function samba_configuration() {
 		/usr/local/samba/bin/samba-tool domain provision  --use-rfc2307 --realm='''$domain''' --domain '''$netbios''' --server-role=dc --adminpass=$domain_password
 		cp /usr/local/samba/private/krb5.conf /etc/krb5.conf
 	fi
+}
+function openvpn_installation() {
+	echo "en cours..."
 }
