4dade363d98d1929d497bf79893eddc857e5475d
Mathieu Raux <mathieu_raux@hotmail.fr>
2019-02-13 19:47:34 +0100

    Modification du script viking.sh pour qu'il commence à comprendre les arguments

--- script/viking.sh
+++ script/viking.sh
@@ -4,17 +4,27 @@
 source ./viking_function.sh
 
 #Présentation et menu
-whiptail --title "Hello, Vicking !" --msgbox "Bienvenue dans le script d'installation de Vicking." 10 60
-MAIN=$(whiptail --title "Que souhaitez vous faire ?" --menu "Choisir une option" 25 78 16 \
-"Installation" "Donner moi quelques informations, et je m'occupe du reste" \
-"Configuration" "Pour configurer ou reconfigurer un service" \
-"Quitter" "Ferme ce menu" 3>&1 1>&2 2>&3)
-
-case $MAIN in
-	"Installation")
-		installmenu
-	;;
-	"Quitter")
-		exit
-	;;
+while getopts ":h" option; do
+	case "${option}" in
+		h)
+			echo "help"
+		;;
 	esac
+done
+
+if [ -z $@ ]; then
+	whiptail --title "Hello, Vicking !" --msgbox "Bienvenue dans le script d'installation de Vicking." 10 60
+	MAIN=$(whiptail --title "Que souhaitez vous faire ?" --menu "Choisir une option" 25 78 16 \
+	"Installation" "Donner moi quelques informations, et je m'occupe du reste" \
+	"Configuration" "Pour configurer ou reconfigurer un service" \
+	"Quitter" "Ferme ce menu" 3>&1 1>&2 2>&3)
+
+	case $MAIN in
+		"Installation")
+			installmenu
+		;;
+		"Quitter")
+			exit
+		;;
+		esac
+fi
