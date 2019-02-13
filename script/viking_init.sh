3baccd1d57e247d2a5a3d24e3556c2e0e4974f52
Mathieu Raux <mathieu_raux@hotmail.fr>
2019-02-13 10:31:30 +0100

    Organisation

---
+++ script/viking_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Include des fonctions
+source ./viking_function.sh
+
+#Présentation et menu
+whiptail --title "Hello, Vicking !" --msgbox "Bienvenue dans le script d'installation de Vicking. Avant de procéder, nous allons définir quelques paramètres ensemble !" 10 60
+
+initmenu
